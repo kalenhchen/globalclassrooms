3c8c39f6c6012aedac43267c394a4eff9241b64f
kalenhchen <135388171+kalenhchen@users.noreply.github.com>
2023-06-07 11:45:57 -0400

    Update all assessments all variables.R

--- all assessments all variables.R
+++ all assessments all variables.R
@@ -17,43 +17,53 @@ FINAL_Free_tuition_and_UBI <- read_xlsx("C:/Users/kalen/OneDrive - University of
 
 # bar plotting
 
-HW_Identifying_Barriers <- HW_Identifying_Barriers %>% select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% mutate(Homework = "HW_Identifying_Barriers")
-HW_Youth_and_Unions <- HW_Youth_and_Unions %>% select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% mutate(Homework = "HW_Youth_and_Unions")
-HW_Womens_March <- HW_Womens_March %>% select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% mutate(Homework = "HW_Womens_March")
-HW_Free_tuition_and_UBI <- HW_Free_tuition_and_UBI %>% select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% mutate(Homework = "HW_Free_tuition_and_UBI")
-FINAL_Youth_and_Unions <- FINAL_Youth_and_Unions %>% select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% mutate(Homework = "FINAL_Youth_and_Unions")
-FINAL_Free_tuition_and_UBI <- FINAL_Free_tuition_and_UBI %>% select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% mutate(Homework = "FINAL_Free_tuition_and_UBI")
+HW_Identifying_Barriers <- HW_Identifying_Barriers %>% 
+  select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% 
+  mutate(Assessment = "Identifying Barriers HW")
+HW_Youth_and_Unions <- HW_Youth_and_Unions %>% 
+  select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% 
+  mutate(Assessment = "Youth and Unions HW")
+HW_Womens_March <- HW_Womens_March %>%
+  select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>%
+  mutate(Assessment = "Women's March HW")
+HW_Free_tuition_and_UBI <- HW_Free_tuition_and_UBI %>% 
+  select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% 
+  mutate(Assessment = "Free tuition and UBI HW")
+FINAL_Youth_and_Unions <- FINAL_Youth_and_Unions %>% 
+  select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% 
+  mutate(Assessment = "Youth and Unions related exam responses")
+FINAL_Free_tuition_and_UBI <- FINAL_Free_tuition_and_UBI %>% 
+  select(Analytic, Clout, Tone, Cognition, Social, Lifestyle, work, Perception, space) %>% 
+  mutate(Assessment = "Free tuition and UBI related exam responses")
 
 # combine to one data set
 all_scores <- rbind(HW_Identifying_Barriers, HW_Youth_and_Unions, HW_Womens_March, HW_Free_tuition_and_UBI, FINAL_Youth_and_Unions, FINAL_Free_tuition_and_UBI)
 all_scores <- all_scores %>% 
-  pivot_longer(-Homework, names_to = "Variable", values_to = "score")
+  pivot_longer(-Assessment, names_to = "Variable", values_to = "score")
 
 # aggregate scores based on dimensions
 library(stringr)
 all_scores$Variable <- str_to_title(all_scores$Variable)
 all_scores <- all_scores %>% mutate(var2 = case_when(Variable == "Analytic"~"Thinking",
-                                                     Variable == "Cognition" ~"Thinking",
-                                                     Variable == "Clout" ~ "Social Leadership",
-                                                     Variable == "Social"  ~ "Social Leadership",
-                                                     Variable == "Lifestyle" ~  "Work and Lifestyle",
-                                                     Variable == "Work" ~  "Work and Lifestyle",
+                                                     Variable == "Cognition"~"Thinking",
+                                                     Variable == "Clout"~"Social Leadership",
+                                                     Variable == "Social"~"Social Leadership",
+                                                     Variable == "Lifestyle"~"Work and Lifestyle",
+                                                     Variable == "Work"~"Work and Lifestyle",
                                                      Variable == "Perception"~"Experiential",
                                                      Variable == "Space"~"Experiential",
-                                                     Variable =="Tone"~"Experiential"
+                                                     Variable == "Tone"~"Experiential"
 ))
 
-all_scores$Variable <- factor(all_scores$Variable, levels =c("Analytic","Cognition", "Clout", "Social", "Lifestyle", "Work", "Perception", "Space", "Tone"))
-
-
+all_scores$Variable <- factor(all_scores$Variable, levels =c("Analytic","Cognition","Clout","Social","Lifestyle","Work","Perception","Space","Tone"))
 
 all_scores %>% 
-  group_by(Variable, Homework) %>% 
+  group_by(Variable, Assessment) %>% 
   mutate(mean = mean(score,na.rm=T)) %>% 
-  mutate(Homework = factor(Homework, levels = c("HW_Identifying_Barriers", "HW_Youth_and_Unions", "HW_Womens_March", "HW_Free_tuition_and_UBI", "FINAL_Youth_and_Unions", "FINAL_Free_tuition_and_UBI"))) %>% 
-  ggplot(aes(x = Variable,  y = mean, fill = Homework)) +
+  mutate(Assessment = factor(Assessment, levels = c("Identifying Barriers HW","Youth and Unions HW","Women's March HW","Free tuition and UBI HW","Youth and Unions related exam responses","Free tuition and UBI related exam responses"))) %>% 
+  ggplot(aes(x = Variable,  y = mean, fill = Assessment)) +
   geom_bar(position = "dodge", stat = "identity") +
-  labs(title = "", caption = "Percentage of words in student responses that fall into select LIWC-22 categories.", x = "Linguistic dimension",  y = "Mean score") +
+  labs(title = "", caption = "Percentage of words in student responses that fall into select LIWC-22 categories.",x = "Linguistic dimension",  y = "Mean score") +
   theme(legend.position = "right") +
   ylim(0, 90) +
   geom_signif(y_position = c(90,80), xmin = c(0.5,2.5), 
